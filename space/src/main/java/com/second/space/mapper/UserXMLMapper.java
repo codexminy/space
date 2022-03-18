109ae02aa3c504d535ba3f0fe9f3df3e5b9a7f43
devxmonica <devxmonica@gmail.com>
2022-03-18 14:40:34 +0900

    Chore UserXMLMapper 주석 처리

--- space/src/main/java/com/second/space/mapper/UserXMLMapper.java
+++ space/src/main/java/com/second/space/mapper/UserXMLMapper.java
@@ -1,9 +1,11 @@
-package com.second.space.mapper;
-
-import com.second.space.user.model.User_DTO;
-
-public interface UserXMLMapper {
-
-	public void userSignUp(User_DTO user);
-
-}
+//package com.second.space.mapper;
+//
+//import com.second.space.user.model.User_DTO;
+//
+//public interface UserXMLMapper {
+//
+//	public void userSignUp(User_DTO user);
+//
+//	public User_DTO login(User_DTO user);
+//
+//}
