09a2930f89f66114057d2a45f6d3dfdee1c31a20
codexminy <codexminy@gmail.com>
2022-03-25 15:48:59 +0900

    깃 충돌

--- space/src/main/java/com/second/space/common/util/Utils.java
+++ space/src/main/java/com/second/space/common/util/Utils.java
@@ -15,4 +15,15 @@ public class Utils {
 		
 		return sdf.format(new Date())+"_"+rndNum+"."+ext;
 	}
+	
+	public static String getRenamedFileName2(String fname) {
+		//확장자 분리
+		String ext = fname.substring(fname.lastIndexOf(".")+1);
+		//날짜
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd_HHmmssSSS");
+		//난수 만들기
+		int rndNum = (int)(Math.random()*1000000000);
+		
+		return sdf.format(new Date())+"_"+rndNum+"."+ext;
+	}
 }
